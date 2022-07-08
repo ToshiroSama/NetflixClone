97e835430a36b932f33dedcd41e435963e6f96ea
ToshiroSama <alishdjuraev@gmail.com>
2022-07-08 13:57:06 +0500

    TableViewController was a successfully created inside of HomeViewController, configured with 2 protocols. CollectionView was created inside of TableViewCell

---
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixClone
+//
+//  Created by Alisher Djuraev on 08/07/22.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        embedSubviews()
+        collectionConfiguration()
+    }
+    
+    private func embedSubviews() {
+        contentView.addSubview(collectionView)
+    }
+    
+    private func collectionConfiguration() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+// MARK: - Collection View Configration, cells, rows and so ...
+
+extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
+
