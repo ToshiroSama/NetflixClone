ccb0ca85586cfe3b71f48a61b592d82b526dd0d8
ToshiroSama <alishdjuraev@gmail.com>
2022-07-13 11:28:33 +0500

    SDWebImage was imported. TitleColletcionViewCell was added for each poster image items in 5 sections

---
+++ NetflixClone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by Alisher Djuraev on 13/07/22.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    public func configure(with model: String) {
+        guard let url = URL(String: model) else { return }
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+}
